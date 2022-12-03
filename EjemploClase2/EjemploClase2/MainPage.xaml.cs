23014f96546f2b70b2ba42964093690e7cb517cf
DELL <DELL@DESKTOP-FUNN0TB>
2022-12-02 18:09:19 -0600

    Agregar archivos de proyecto.

---
+++ EjemploClase2/EjemploClase2/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EjemploClase2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnEnviar_Clicked(object sender, EventArgs e)
+        {
+            var atributo = new Models.Atributos
+            {
+                Nombres = TxtNombres.Text,
+                Apellidos = TxtApellidos.Text,
+                Edad = Convert.ToInt32(TxtEdad.Text),
+                Correo = TxtCorreo.Text,
+                EnviarNombre = TxtNombres.Text, 
+                EnviarApellido = TxtApellidos.Text,
+                EnviarEdad = Convert.ToInt32(TxtEdad.Text),
+                EnviarCorreo = TxtCorreo.Text
+            };
+
+            var enviar = new Views.Enviar();
+            enviar.BindingContext = atributo;
+            await Navigation.PushModalAsync(enviar);
+        }
+    }
+}
---
+++ EjemploClase2/EjemploClase2/Models/Atributos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploClase2.Models
+{
+    public class Atributos
+    {
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public int Edad { get; set; }
+        public string Correo { get; set; }
+        public string EnviarNombre { get; set; }
+        public string EnviarApellido { get; set; }
+        public int EnviarEdad { get; set; }
+        public string EnviarCorreo { get; set; }
+    }
+}
